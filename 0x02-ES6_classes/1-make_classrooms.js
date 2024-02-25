09b3195c77cbf85c32bd2a5bd3bef41b12da3ab1
PMagombedze <percymagoras@outlook.com>
2024-02-25 19:55:08 +0200

    initialize classroom

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,9 @@
 import ClassRoom from "./0-classroom.js";
 
-export default function initializeRooms() {
-  let sizes = [19, 20, 34];
-  return sizes.map((size) => new ClassRoom(size));
+function initializeRooms() {
+  const sizes = [19, 20, 34];
+  const rooms = sizes.map((size) => new ClassRoom(size));
+  return rooms;
 }
+
+export default initializeRooms;
