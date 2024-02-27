e0914a932a504088f328d76de76bd61ff66894b6
PMagombedze <percymagoras@outlook.com>
2024-02-27 22:29:57 +0200

    hello nodejs!

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Prints given message to STDOUT.
+ * @param {string} message - str arg.
+ */
+function displayMessage (message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
