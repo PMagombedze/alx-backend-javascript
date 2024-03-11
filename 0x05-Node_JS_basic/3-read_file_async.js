4dbe654d42e04426ecc3619247751ad2e846ba13
PMagombedze <percymagoras@outlook.com>
2024-03-11 22:05:53 +0200

    apply semistandard

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,19 +1,19 @@
-const fs = require("fs");
+const fs = require('fs');
 
-async function countStudents(path) {
+async function countStudents (path) {
   try {
-    const files = await fs.promises.readFile(path, "utf8");
-    const file = files.split("\n");
+    const files = await fs.promises.readFile(path, 'utf8');
+    const file = files.split('\n');
 
     const myArr = [];
     const myArrTwo = [];
 
     file.forEach((record) => {
-      const field = record.split(",");
+      const field = record.split(',');
       if (field.length > 0) {
-        if (field[3] === "CS") {
+        if (field[3] === 'CS') {
           myArr.push(field[0]);
-        } else if (field[3] === "SWE") {
+        } else if (field[3] === 'SWE') {
           myArrTwo.push(field[0]);
         }
       }
@@ -21,15 +21,15 @@ async function countStudents(path) {
 
     console.log(`Number of students: ${myArr.length + myArrTwo.length}`);
     console.log(
-      `Number of students in CS: ${myArr.length}. List: ${myArr.join(", ")}`
+      `Number of students in CS: ${myArr.length}. List: ${myArr.join(', ')}`
     );
     console.log(
       `Number of students in SWE: ${myArrTwo.length}. List: ${myArrTwo.join(
-        ", "
+        ', '
       )}`
     );
   } catch (err) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
 }
 
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,45 +1,45 @@
-const fs = require("fs");
-const express = require("express");
+const fs = require('fs');
+const express = require('express');
 
 const app = express();
 const port = 1245;
 
-function countStudents(path) {
+function countStudents (path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, "utf8", (err, records) => {
-      if (err) reject(new Error("Cannot load the database"));
+    fs.readFile(path, 'utf8', (err, records) => {
+      if (err) reject(new Error('Cannot load the database'));
       else {
-        const files = records.split("\n");
+        const files = records.split('\n');
         const myArr = [];
         const myArrTwo = [];
 
         files.forEach((file) => {
-          const field = file.split(",");
+          const field = file.split(',');
           if (field.length > 0) {
-            if (field[3] === "CS") {
+            if (field[3] === 'CS') {
               myArr.push(field[0]);
-            } else if (field[3] === "SWE") {
+            } else if (field[3] === 'SWE') {
               myArrTwo.push(field[0]);
             }
           }
         });
 
         let str = `Number of students: ${myArr.length + myArrTwo.length}\n`;
-        str += `Number of students in CS: ${myArr.length}. List: ${myArr.join(", ")}\n`;
-        str += `Number of students in SWE: ${myArrTwo.length}. List: ${myArrTwo.join(", ")}`;
+        str += `Number of students in CS: ${myArr.length}. List: ${myArr.join(', ')}\n`;
+        str += `Number of students in SWE: ${myArrTwo.length}. List: ${myArrTwo.join(', ')}`;
         resolve(str);
       }
     });
   });
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello Holberton School!");
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.get("/students", async (req, res) => {
-  res.write("This is the list of our students\n");
-  const database = process.argv.length > 2 ? process.argv[2] : "";
+app.get('/students', async (req, res) => {
+  res.write('This is the list of our students\n');
+  const database = process.argv.length > 2 ? process.argv[2] : '';
   try {
     const files = await countStudents(database);
     res.write(files);
