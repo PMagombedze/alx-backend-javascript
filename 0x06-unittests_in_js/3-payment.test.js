45d6c089482ddaea932a7a895f6bb8b69d898658
PMagombedze <percymagoras@outlook.com>
2024-03-25 23:18:50 +0200

    3-payment

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 const sinon = require('sinon');
-const Utils = require('./Utils');
+const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToAp', function() {
@@ -13,15 +13,10 @@ describe('sendPaymentRequestToAp', function() {
   });
   it("validate the usage of the `Utils.calculateNumber` function", function() {
     sendPaymentRequestToApi(100, 20);
-    // Check it is called
     expect(Utils.calculateNumber.called).to.be.true;
-    // Check it is called once
     expect(Utils.calculateNumber.calledOnce).to.be.true;
-    // Check call count
     expect(Utils.calculateNumber.callCount).to.be.equal(1);
-    // Check it first argument
     expect(Utils.calculateNumber.firstCall.args[0]).to.equal("SUM");
-    // Check params
     expect(Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
   });
 });
